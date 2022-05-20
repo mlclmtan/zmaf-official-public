0836471d077a4d84beba4435efe95b3f80d70619
Malcolm Tan <malcolm.tan@acronis.com>
2022-05-21 04:18:06 +0800

    public

---
+++ js/lang/cn.js
@@ -0,0 +1,37 @@
+//data-i18n="company_cn"
+const i18nLangs = {
+  nav_home: '主页',
+  nav_company: '我们公司',
+  nav_factory: '厂房',
+  nav_products: '产品',
+  nav_contact: '联络',
+  btn_contact: '联络我们',
+  main_1st: '我 们 是 拥 有 顶 尖 科 技 的<br>电 子 烟 制 造 厂',
+  main_2nd: '我们提供电子产品OEM定制服务，如电子烟雾化器、电子烟、数码录音笔等',
+  tech_health: '让科技带给您健康',
+  elec_cig: '电子烟雾化器、电子烟、数码录音笔',
+  company_cn: '深圳市中马阿珐电子科技有限公司',
+  our_company_des:
+    '为保证所生产的产品符合国家的标准，本公司拥有专业的技术研发团队及先进的生产设备。本公司凭借自身的技术实力和专业精神，及对相关领域的资源有效的整合与协作，已在创新中持续且稳定地快速发展。',
+  customer_first1: '客户',
+  customer_first2: '至上',
+  customer_first_des:
+    '中马阿珐一直以来都本着“用户第一、质量致上、锐意进取、开拓创新、追求卓越”的宗旨及理念发展，为市场及消费者推出多种技术先进的优良产品。我们在不断创新的同时也仍然坚守着“质量至上”的原则，因此有幸收获大批客户的信赖并获得“品质保证、客户至上”的美誉。',
+  our_prod: '我们特色商品',
+  add: '地址',
+  add_long:
+    '深圳市中马阿珐电子科技有限公司<br>深圳市石岩园岭志泫翰工业园I栋3楼',
+  contact_long: '欢迎透过手机号码、电子邮件或微信联络我们',
+  contact_phone: '联络号码： +(86) 13923738586',
+  contact_email: '电邮： 794315867@qq.com',
+  contact_wechat: '微信：',
+  Devices: '烟杆',
+  Refill_Pods: '烟弹',
+  D36_Disposable_Vape: 'D36 一次性电子烟',
+  Huo_Niu_Device: '火牛烟杆',
+  _2M_Vape: '2M 扁烟',
+  Gum_Size_Disposable_Vape: '一次性电子烟',
+  CBD_Atomizer_with_Ceramic_Core: '陶瓷芯CBD雾化器',
+  _W31_Transparent: 'W31 透明烟弹',
+  W31 : 'W31',
+};
